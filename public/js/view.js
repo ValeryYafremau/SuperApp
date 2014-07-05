7c2c840d95f85486081a255628c50b8e5fa827cc
ValeryYafremau <vaefremov.gomel@gmail.com>
2014-07-05 13:16:04 +0300

    Update view.js

--- public/js/view.js
+++ public/js/view.js
@@ -2,16 +2,13 @@ var TasksView = Backbone.View.extend( {
 
 	el : $("#list"),
 
-	activeTemplate : _.template("<div class ='task'><a href = '#note/<%= id %>' class='x'>X</a><input type = 'checkbox' name = 'select' id = '<%= id %>'> <%= task %></div"),
-
-	complitedTemplate : _.template("<div class ='task'><%= task %></div"),
+	template : _.template("<div class ='task'><a href = '#note/<%= id %>' class='x'>X</a><input type = 'checkbox' name = 'select' id = '<%= id %>'> <%= task %></div"),
 
 	render : function(tasks) {
-		var template = ($('input:radio[name=status]:checked').val() === "active") ? this.activeTemplate : this.complitedTemplate;
 		var that = this;
 		this.$el.empty();
 		_.each(tasks, function(task) {
-			that.$el.prepend(template(task));
+			that.$el.prepend(that.template(task));
 		});
 		$('#selector').prop("checked", false);
 		$('#selector').trigger("change");
