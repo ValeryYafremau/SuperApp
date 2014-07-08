775a26f8250395f14818c042a7ba8d710b285a8a
ValeryYafremau <vaefremov.gomel@gmail.com>
2014-07-09 00:10:32 +0300

    Update view.js

--- public/js/view.js
+++ public/js/view.js
@@ -7,13 +7,8 @@ var TasksView = Backbone.View.extend( {
 	render : function(tasks) {
 		var that = this;
 		this.$el.empty();
-<<<<<<< HEAD
 		_.each(tasks.models, function(task) {
 			that.$el.prepend(that.template(task.toJSON()));
-=======
-		_.each(tasks, function(task) {
-			that.$el.prepend(that.template(task));
->>>>>>> d851f4532ac2517da978fed00239473c16a55d84
 		});
 		$('#selector').prop("checked", false);
 		$('#selector').trigger("change");
