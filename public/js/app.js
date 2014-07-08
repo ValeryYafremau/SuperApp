cee3266f5e3a2efdbc2619822bd4b9ad16d8d960
ValeryYafremau <vaefremov.gomel@gmail.com>
2014-07-09 00:09:36 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var Task = Backbone.Model.extend({
 	urlRoot : "/"
 });
@@ -11,8 +10,6 @@ var Tasks = Backbone.Collection.extend({
 });
 
 var tasks = new Tasks();
-=======
->>>>>>> d851f4532ac2517da978fed00239473c16a55d84
 
 var tasksView = new TasksView();
 
@@ -23,13 +20,8 @@ Backbone.history.start();
 $(document).ready(function() {
 
     $('input:radio[name=status]').change(function(){  
-<<<<<<< HEAD
 	$('#complete').text($('input:radio[name=status]:checked').val() === 'active' ? 'Complete' : 'Delete');
 	tasksRouter.navigate("#filter/" + $('input[name=status]:checked').val(), {trigger: true});  
-=======
-	$('#complite').text($('input:radio[name=status]:checked').val() === 'active' ? 'Complete' : 'Delete');
-	tasksRouter.filter($('input:radio[name=status]').val());
->>>>>>> d851f4532ac2517da978fed00239473c16a55d84
     });
 
     $('#form').submit(function(e) {
